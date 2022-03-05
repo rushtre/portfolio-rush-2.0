2ced672edb07d631ad7396c75d0653fa50fbb358
solera2 <rushtre1@gmail.com>
2022-03-04 23:22:52 -0500

    initial commit and files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
