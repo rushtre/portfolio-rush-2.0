7a90a8e0b31e9cad4994ebd2027c0a9f640b7b20
solera2 <rushtre1@gmail.com>
2022-03-05 13:48:07 -0500

    completed header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Info from './Info';
 import Me from '../../assets/me21.png'
+import Socials from './Socials';
 import './header.css';
 
 const Header = () => {
@@ -10,10 +11,11 @@ const Header = () => {
                 <h1>Tre' Rush</h1>
                 <h5 className='text-light'>Fullstack Developer</h5>
                 <Info />
-
-                <div>
+                <Socials />
+                <div className='me'>
                     <img src={Me} alt='me' />
                 </div>
+
             </div>
         </header>
     )
--- src/components/Header/Socials.js
+++ src/components/Header/Socials.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { FiDribbble } from 'react-icons/fi';
 
 const Socials = () => {
     return (
-        <div>Socials</div>
+        <div className='socials'>
+            <a href='https://linkedin.com' target='_blank'><BsLinkedin /></a>
+            <a href='https://github.com' target='_blank'><FaGithub /></a>
+            <a href='https://dribbble.com' target='_blank'><FiDribbble /></a>
+        </div>
     )
 };
 
