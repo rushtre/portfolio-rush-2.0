2f56e0f4577b2a37e7c292dca1f59a01745d22f4
solera2 <rushtre1@gmail.com>
2022-03-05 23:30:19 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import Experience from './components/Experience/Experience';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import { useState } from 'react';
 
 
 function App() {
+
+
   return (
     <>
       <Header />
